2447dac30288d1f67aa5e2036f32b291d4bd1822
Medina <murilo.medina@ibbx.tech>
2025-03-11 18:09:11 -0300

    Add dropdown menu to Navbar and update dependencies

--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -1,21 +1,59 @@
 import { Bell } from "lucide-react";
-import { ActionsContainer, Logo, NavContainer, NotificationButton } from "./styles";
+import {
+  ActionsContainer,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  Logo,
+  MenuButton,
+  NavContainer,
+  NotificationButton,
+} from "./styles";
 import { ProfileAvatar } from "../Avatar";
 import logo from "../../../public/logo.png";
+import { DropdownMenu } from "radix-ui";
 
-export function Navbar() {
-    return (
-        <NavContainer>
-            <Logo>
-                <img src={logo} />
-                | WeWrite
-            </Logo>
-            <ActionsContainer>
-                <NotificationButton>
-                    <Bell size={20} />
-                </NotificationButton>
+interface NavbarProps {
+  onOpenNotification: () => void;
+}
+
+export function Navbar({ onOpenNotification }: NavbarProps) {
+  return (
+    <NavContainer>
+      <Logo>
+        <img src={logo} />| WeWrite
+      </Logo>
+      <ActionsContainer>
+        <NotificationButton onClick={onOpenNotification}>
+          <Bell size={20} />
+        </NotificationButton>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <MenuButton>
+              <ProfileAvatar />
+            </MenuButton>
+          </DropdownMenu.Trigger>
+          <DropdownMenuPortal>
+            <DropdownMenuContent align="end" sideOffset={5}>
+              <div className="d-flex align-items-center gap-2 p-2">
                 <ProfileAvatar />
-            </ActionsContainer>
-        </NavContainer>
-    )
+                <div className="d-flex flex-column">
+                  <span className="fs-6 fw-medium">Nome teste</span>
+                </div>
+              </div>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenuPortal>
+        </DropdownMenu.Root>
+      </ActionsContainer>
+    </NavContainer>
+  );
 }
--- src/components/ui/Navbar/styles.ts
+++ src/components/ui/Navbar/styles.ts
@@ -1,46 +1,106 @@
+import { DropdownMenu } from "radix-ui";
 import styled from "styled-components";
 
 export const NavContainer = styled.nav`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    padding: 0 2rem;
-    height: 5rem;
-    background-color: ${({ theme }) => theme.colors.background};
-    border-bottom: 1px solid ${({ theme }) => theme.colors.border};
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 2rem;
+  height: 5rem;
+  background-color: ${({ theme }) => theme.colors.background};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+`;
 
 export const Logo = styled.span`
-    display: flex;
-    align-items: center;
-    font-size: 1.25rem;
-    font-weight: 700;
-    gap: 0.5rem;
-    
-    img {
-        height: 2.5rem;
-    }
-`
+  display: flex;
+  align-items: center;
+  font-size: 1.25rem;
+  font-weight: 700;
+  gap: 0.5rem;
+
+  img {
+    height: 2.5rem;
+  }
+`;
 
 export const ActionsContainer = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 0.875rem;
-`
+  display: flex;
+  align-items: center;
+  gap: 0.875rem;
+`;
 
 export const NotificationButton = styled.button`
-    all: unset;
-    width: 2.25rem;
-    height: 2.25rem;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    transition: background ease-in-out 0.1s;
+  all: unset;
+  width: 2.25rem;
+  height: 2.25rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: background ease-in-out 0.1s;
 
-    &:hover {
-        background-color: ${({ theme }) => theme.colors.accent};
-    }
-`
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.accent};
+  }
+`;
+
+export const MenuButton = styled.button`
+  all: unset;
+`;
+
+export const DropdownMenuContent = styled(DropdownMenu.Content)`
+  z-index: 50;
+  min-width: 8rem;
+  overflow: hidden;
+  border-radius: 0.375rem;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  background-color: ${({ theme }) => theme.colors.popover};
+  color: ${({ theme }) => theme.colors.popoverForeground};
+  padding: 0.25rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+`;
+
+export const DropdownMenuPortal = styled(DropdownMenu.Portal)``;
+
+export const DropdownMenuItem = styled(DropdownMenu.Item)`
+  position: relative;
+  display: flex;
+  user-select: none;
+  align-items: center;
+  border-radius: 0.25rem;
+  padding: 0.375rem 0.5rem;
+  font-size: 0.875rem;
+  outline: none;
+  transition: background-color 0.2s, color 0.2s;
+  cursor: pointer;
+
+  :focus {
+    background-color: ${({ theme }) => theme.colors.accent};
+    color: ${({ theme }) => theme.colors.accentForeground};
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.accent};
+    color: ${({ theme }) => theme.colors.accentForeground};
+  }
+
+  &[data-disabled] {
+    pointer-events: none;
+    opacity: 0.5;
+  }
+
+  .inset {
+    padding-left: 2rem;
+  }
+`;
+
+export const DropdownMenuSeparator = styled(DropdownMenu.Separator)`
+  margin-left: -0.25rem;
+  margin-right: -0.25rem;
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+  height: 1px;
+  background-color: ${({ theme }) => theme.colors.muted};
+`;
